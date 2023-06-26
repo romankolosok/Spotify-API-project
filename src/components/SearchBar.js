74356ecbf53a91449a6d351a213b7f18406316db
Roman Kolosok <romakolosok14@gmail.com>
2023-06-26 18:32:29 -0400

    interim commit #1

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+export default function SearchBar() {
+
+    return (
+        <form>
+            <input type="text"/>
+            <button type="submit">SEARCH</button>
+        </form>
+    )
+}
